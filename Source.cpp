c78ffdfd1bb1b20f6beb2a113554b33a4eb06937
Archer <pristinskiyd@gmail.com>
2021-08-27 17:34:08 +0300

    Fix logic bug and partly add dynamic field size(Input through console)

--- Source.cpp
+++ Source.cpp
@@ -6,16 +6,17 @@ using namespace sf;
 
 int main()
 {
-	const int FIELD_NUM = 10, WIDTH = 32;
+	int FIELD_NUM = 4; const int WIDTH = 32;
 	srand(time(0));
 	RenderWindow window(VideoMode(400, 400), "Miner");
-
+	std::cout << "Enter field size: ";
+	std::cin >> FIELD_NUM;
 	Texture t;
 	t.loadFromFile("image/sprite.jpg");
 	Sprite s(t);
 
-	int gridLogic[FIELD_NUM + 2][FIELD_NUM + 2];//bigger array was taken to prevent out of range,when init mines and numbers
-	int gridView[FIELD_NUM + 2][FIELD_NUM + 2];
+	std::vector<std::vector<int>> gridLogic(FIELD_NUM + 2, std::vector<int>(FIELD_NUM + 2));//bigger array was taken to prevent out of range,when init mines and numbers
+	std::vector<std::vector<int>> gridView(FIELD_NUM + 2, std::vector<int>(FIELD_NUM + 2));
 	/*creating mines in 1 to FIELD_NUM - 2 range(0 -non-active field,1-active field)
 	* {
 	*	0	0	0	0	0	0
@@ -26,16 +27,17 @@ int main()
 	*	0	0	0	0	0	0
 	* }
 	*/
-	for (int i = 1; i <= FIELD_NUM - 2; i++)
-		for (int j = 1; j <= FIELD_NUM - 2; j++)
+	for (int i = 1; i <= FIELD_NUM; i++)
+		for (int j = 1; j <= FIELD_NUM; j++)
 		{
 			gridView[i][j] = 10;
 			if (rand() % 5 == 0)gridLogic[i][j] = 9;
 			else gridLogic[i][j] = 0;
 
 		}
-	for (int i = 1; i <= FIELD_NUM - 2; i++)
-		for (int j = 1; j <= FIELD_NUM - 2; j++)
+
+	for (int i = 1; i <= FIELD_NUM ; i++)
+		for (int j = 1; j <= FIELD_NUM; j++)
 		{
 			int n = 0;
 			if (gridLogic[i][j] == 9) continue;
@@ -52,37 +54,44 @@ int main()
 
 	while (window.isOpen())
 	{
-		Vector2i pos = Mouse::getPosition(window);
-		int x = pos.x / WIDTH;
-		int y = pos.y / WIDTH;
-
 		Event e;
-
-		while (window.pollEvent(e))
-		{
-			if (e.type == Event::Closed)
-				window.close();
-
-			// Определяем, была ли нажата кнопка мыши?
-			if (e.type == Event::MouseButtonPressed)
-
-				// Если была нажата левая кнопка мыши, то открываем клетку
-				if (e.key.code == Mouse::Left) gridView[x][y] = gridLogic[x][y];
-
-			// Если была нажата правая кнопка мыши, то отображаем флажок
-				else if (e.key.code == Mouse::Right) gridView[x][y] = 11;
-		}
 		window.clear(Color::White);
-		for (int i = 1; i <= FIELD_NUM - 2; i++)
-			for (int j = 1; j <= FIELD_NUM - 2; j++)
+		for (int i = 1; i <= FIELD_NUM; i++)
+			for (int j = 1; j <= FIELD_NUM; j++)
 			{
-				if (gridView[x][y] == 9) gridView[i][j] = gridLogic[i][j];
+
 				s.setTextureRect(IntRect(gridView[i][j] * WIDTH, 0, WIDTH, WIDTH));
 				s.setPosition(i * WIDTH, j * WIDTH);
 				window.draw(s);
 			}
 
 		window.display();
+		while (window.pollEvent(e))
+		{
+			Vector2i pos = Mouse::getPosition(window);
+			int x = pos.x / WIDTH;
+			int y = pos.y / WIDTH;
+
+			if (e.type == Event::Closed)
+				window.close();
+
+			if (e.type == Event::MouseButtonPressed)
+			{
+				if (e.key.code == Mouse::Left)
+				{
+					gridView[x][y] = gridLogic[x][y];
+					if (gridLogic[x][y] == 9)
+						for (int i = 1; i <= FIELD_NUM; i++)
+							for (int j = 1; j <= FIELD_NUM; j++)
+							{
+								gridView[i][j] = gridLogic[i][j];
+							}
+				}
+				else if (e.key.code == Mouse::Right) gridView[x][y] = 11;
+				break;
+			}
+		}
+			
+		}
+		return 0;
 	}
-	return 0;
-}
